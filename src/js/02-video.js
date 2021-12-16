a2272dcd48e94a22e29877f9e4ac93d67e1fe708
RuslanVidnichuk <monnaliza139@gmail.com>
2021-12-16 14:45:35 +0200

    finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+startCurrentTime();
+
+player.on('timeupdate', Throttle(function(data) {
+    const currentTime = data.seconds;
+    console.log(currentTime)
+
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
+    
+function startCurrentTime() {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import Throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+let feedbackFormData = {};
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', Throttle(onFormInput, 500));
+
+repairFormsData();
+
+function onFormInput(evt) {
+    feedbackFormData[evt.target.name] = evt.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormData));
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    feedbackFormData = {};
+};
+
+function repairFormsData() {
+    const saveFeedbackFormData = localStorage.getItem('feedback-form-state');
+    const parsedFeedbackFormData = JSON.parse(saveFeedbackFormData);
+    if (parsedFeedbackFormData) {
+        form.email.value = parsedFeedbackFormData.email;
+        form.message.value = parsedFeedbackFormData.message;
+
+        if (!parsedFeedbackFormData.email) {
+            form.email.value = '';
+        };
+        if (!parsedFeedbackFormData.message) {
+            form.message.value = '';
+        }
+
+    };
+    feedbackFormData = { ...parsedFeedbackFormData };
+}
